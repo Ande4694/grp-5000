41518ceae7dd303726c26d29bce21ae63090ab8e
Anders Knudsen <knudsen879@hotmail.com>
2018-03-01 10:58:44 +0100

    backup før implementering af multi gameover variabler

--- Anders/Game.java
+++ Anders/Game.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /*            case 1:        ///kanin op     
                // metode til at udregne afstanden mellem rabbit(x,y) og snake (x,y)
                // http://www.mathwarehouse.com/algebra/distance_formula/index.php 
-               double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));
+               // double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));
                
                rabbit.move(1,0);
                
@@ -39,7 +39,7 @@ import java.util.Scanner;
                ///kanin ned     
                // metode til at udregne afstanden mellem rabbit(x,y) og snake (x,y)
                // http://www.mathwarehouse.com/algebra/distance_formula/index.php 
-               double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));
+               // double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));
                
                rabbit.move(-1,0);
                
@@ -74,7 +74,7 @@ import java.util.Scanner;
                ///kanin venstre     
                // metode til at udregne afstanden mellem rabbit(x,y) og snake (x,y)
                // http://www.mathwarehouse.com/algebra/distance_formula/index.php 
-               double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));
+               // double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));
                
                rabbit.move(0,-1);
                
@@ -109,7 +109,7 @@ import java.util.Scanner;
                ///kanin højre     
                // metode til at udregne afstanden mellem rabbit(x,y) og snake (x,y)
                // http://www.mathwarehouse.com/algebra/distance_formula/index.php 
-               double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));
+               // double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));
                
                rabbit.move(0,1);
                
@@ -156,11 +156,33 @@ public class Game{
       Point rabbit = new Point(0,0);
    // der laves et punkt der representere slangen
       Point snake = new Point(10,10);
+      double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));
       snake.sound = "evil";
       rabbit.sound = "nice";
       rabbit.name = "rabbit";
       snake.name = "snake";
       int turns = 0;
       
+      Scanner input = new Scanner(System.in);
+      System.out.println("Welcome to group 5000's Little Animal Horror Zoo");
+      System.out.println("Your goal is to avoid the snake for 16 turns");
+      System.out.println("To control the rabbit, choose one of the  following commands");
+      menuPrompt();
+      
+      int choice;
+      do 
+      {
+         choice = input.nextInt();
+         menuPrompt();
+      }while(choice !=0); /* || turns<16 || distb < 2)  // turns // 0 give up // distb < 2    */
+      System.out.println("Thank you for playing");
+  }
+  private static void menuPrompt()
+  {
+               System.out.println("\n1: Move rabbit up");
+               System.out.println("2: Move rabbit down");
+               System.out.println("3: Move rabbit left");
+               System.out.println("4: Move rabbit right");
+               System.out.println("0: Give up");
   }
 }
